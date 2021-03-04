d902911e8a3d8a350811a9064d142dd2b1e6039b
Chris Brandt <cmb199@zips.uakron.edu>
2021-03-03 22:45:18 -0600

    Completed test to verify traits are working properly.

--- tests/concepts.cxx
+++ tests/concepts.cxx
@@ -23,8 +23,8 @@ int main()
 
   nola::Integer auto r3 = bar(1, 2);
 
-  //nola::Real    auto r4 = foo(2,   1  );
-  //nola::Integer auto r5 = bar(1., 2.);
+  //nola::Real    auto r4 = foo(1,  2 ); // ERROR!
+  //nola::Integer auto r5 = bar(1., 2.); // ERROR!
 
   std::cout << "\nr1 = " << r1 // = 7
             << "\nr2 = " << r2 // = 7.31
--- tests/traits.cxx
+++ tests/traits.cxx
@@ -5,13 +5,15 @@
 #include <nola/internal/traits.hxx>
 
 
-template <class Real>
-Real foo(Real x, Real y)
+template <class R>
+std::enable_if_t<nola::is_real_v<R>, R>
+foo(R x, R y)
 { return x * y + 5; }
 
 
-template <class Integer>
-Integer bar(Integer x, Integer y)
+template <class I>
+std::enable_if_t<nola::is_integer_v<I>, I>
+bar(I x, I y)
 { return x + y * 5; }
 
 
@@ -23,8 +25,8 @@ int main()
 
   auto r3 = bar(1, 2);
 
-  //auto r4 = foo(2,   1  );
-  //auto r5 = bar(1., 2.);
+  //auto r4 = foo(1,  2 ); // ERROR!
+  //auto r5 = bar(1., 2.); // ERROR!
 
   std::cout << "\nr1 = " << r1 // = 7
             << "\nr2 = " << r2 // = 7.31

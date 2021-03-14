0076ba1d5ae7ab7202811287b1ce93744220c9cb
Chris Brandt <cmb199@zips.uakron.edu>
2021-03-13 18:28:20 -0600

    Added example programs for <stat.hxx> to be used down the road.

---
+++ examples/stat/avx2_float_mean.cxx
@@ -0,0 +1,13 @@
+// Copyright (c) 2019-2021 Christopher M. Brandt
+// All rights reserved
+
+#include <iostream>
+#include <vector>
+#include <nola/simd.hxx>
+
+
+int main()
+{
+  std::cout << "\nSIMD AVX2 Float Mean Example." << std::endl;
+
+}

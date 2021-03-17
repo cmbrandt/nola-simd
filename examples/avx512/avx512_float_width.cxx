94141459b64aa032edecefdb4d240067f4ce5f51
Chris Brandt <cmb199@zips.uakron.edu>
2021-03-17 14:31:22 -0500

    Commiting changes to AVX512 example programs, plus new examples for nola::stat simd implementation.

--- examples/avx512/avx512_float_width.cxx
+++ examples/avx512/avx512_float_width.cxx
@@ -10,7 +10,7 @@ int main()
   std::cout << "\nSIMD AVX512 Width Width Example." << std::endl;
 
   // Compute length of SIMD object
-  constexpr std::int32_t w = nola::simd::avx512_width<int, float>();
+  constexpr std::int32_t w = nola::simd::avx512_width<std::int32_t, float>();
 
   // Display result
   std::cout << "\nwidth = " << w << std::endl; // w = 16

f273a2b8d014f10f692d62913297e420adc7c046
Chris Brandt <cmb199@zips.uakron.edu>
2021-03-20 22:39:50 -0500

    Creating class to encapsulate PRNG for SIMD stat examples.g

--- examples/simd_stat/stat_prng.hxx
+++ examples/simd_stat/stat_prng.hxx
@@ -5,7 +5,25 @@
 #define NOLA_PRNG_HXX
 
 #include <random>
+#include <nola/internal/concepts.hxx>
 
+
+template <nola::Integer I, nola::Real R>
+class Random_values {
+
+  using Generator    = std::mt19937;
+  using Distribution = std::normal_distribution<R>;
+
+public:
+  Random_values() = default;
+  Random_values(I seed) : gen(seed) { }
+
+  R operator()() { return dist(gen); }
+
+private:
+  Generator    gen{std::random_device{}()};
+  Distribution dist{0.0, 1.0};
+};
 
 
 #endif
